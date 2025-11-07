b977dd2202fbef1c9915b2feb04049b6436d72e4
Vinícius de Paula Moraes <manoseilacara123@gmail.com>
2025-11-06 22:26:01 -0300

    feat: initial commit

---
+++ src/main/java/com/mindp/connectus_api/ConnectusApiApplication.java
@@ -0,0 +1,13 @@
+package com.mindp.connectus_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectusApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConnectusApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mindp/connectus_api/ConnectusApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mindp.connectus_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnectusApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
