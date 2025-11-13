89c12b0129b5a0a74533f65621b2c5541a4f615b
Vinícius de Paula Moraes <manoseilacara123@gmail.com>
2025-11-13 14:39:39 -0300

    feat: add websocket to project

---
+++ src/main/java/com/mindp/connectus_api/domain/websocket/WebSocketService.java
@@ -0,0 +1,26 @@
+package com.mindp.connectus_api.domain.websocket;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class WebSocketService {
+
+    private final SimpMessagingTemplate messagingTemplate;
+
+    public void sendToUser(UUID userId, String destination, Object payload) {
+        messagingTemplate.convertAndSendToUser(
+                userId.toString(),
+                destination,
+                payload
+        );
+    }
+
+    public void sendToTopic(String topic, Object payload) {
+        messagingTemplate.convertAndSend(topic, payload);
+    }
+}
