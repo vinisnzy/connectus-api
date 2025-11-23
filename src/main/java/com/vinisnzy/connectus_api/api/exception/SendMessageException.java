56590597c4b3d46e5dff48200381f79c4163ae5b
Vinícius de Paula Moraes <manoseilacara123@gmail.com>
2025-11-23 13:34:41 -0300

    feat: create exception for send message

---
+++ src/main/java/com/vinisnzy/connectus_api/api/exception/SendMessageException.java
@@ -0,0 +1,7 @@
+package com.vinisnzy.connectus_api.api.exception;
+
+public class SendMessageException extends RuntimeException {
+    public SendMessageException(String message) {
+        super(message);
+    }
+}
